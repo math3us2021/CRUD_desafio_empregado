b627d85686adf8f6445ffb5278dd81da91c940f1
math3us2021 <88463383+math3us2021@users.noreply.github.com>
2022-08-17 09:10:58 -0300

    Delete src/src directory

--- src/src/model.ts
+++
@@ -1,6 +0,0 @@
-import {connect} from "./database/index";
-
-export async function selectCustomer(calback:any ): Promise<void> {
-    const conn = await connect();
-     conn.query('SELECT * FROM `tb_empregado`',calback);
-}
